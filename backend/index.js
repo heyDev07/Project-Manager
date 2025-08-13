db76e64c8662751c3b1f19dd2f4aa05014b4e04d
heyDev07 <295devraj@gmail.com>
2025-08-13 22:03:46 +0530

    done

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import cors from "cors";
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
